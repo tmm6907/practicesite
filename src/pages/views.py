34e9e7cf7fcc000ac2382c3299545216aedd6964
Terrence Moore <tmm6907@gmail.com>
2020-08-21 17:02:54 -0400

    pages/views Edit

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home_view(request, *args, **kwargs):
+    my_context = {
+        "Name": "Terrence Moore",
+        "Email": "tmm6907@gmail.com",
+    }
+
+    return render(request, "home.html", my_context)
